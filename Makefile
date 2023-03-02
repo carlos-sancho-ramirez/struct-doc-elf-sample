72c01427a575369309ad91f8aacf1f3e58abfaad
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2023-03-02 22:01:26 +0100

    Include parsing of HeaderX

--- Makefile
+++ Makefile
@@ -1,20 +1,13 @@
-build/bin/elf_sample: src/*.c build/include/elf_structs.h build/bin
+build/bin/elf_sample: src/*.c build/include/elf_structs.h
+	mkdir -p build/bin
 	cc -o $@ -Ibuild/include src/*.c
 
-build/include/elf_structs.h: build/include
+build/include/elf_structs.h:
+	mkdir -p build/include
 	../struct-doc-instancer/build/instancer --in-template ../struct-doc/elf/Header > $@
 	../struct-doc-instancer/build/instancer --in-template ../struct-doc/elf/HeaderX --in-types "w16=word16LittleEndian;w32=word32LittleEndian;addr=word64LittleEndian;offset=word64LittleEndian" >> $@
 	../struct-doc-instancer/build/instancer --in-template ../struct-doc/elf/ProgramEntry --in-types "w32=word32LittleEndian;w32Just32=void;w32Just64=word32LittleEndian;addr=word64LittleEndian;offset=word64LittleEndian" >> $@
 	../struct-doc-instancer/build/instancer --in-template ../struct-doc/elf/SectionEntry --in-types "stringOffset=word64LittleEndian;flags=word64LittleEndian;w32=word32LittleEndian;addr=word64LittleEndian;offset=word64LittleEndian" >> $@
-
-build/include: build
-	mkdir $@
-
-build/bin: build
-	mkdir $@
-
-build:
-	mkdir $@
 
 clean:
 	rm -r build
--- src/elf_sample.c
+++ src/elf_sample.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "elf_structs.h"
 
 #define HEADER_FILE_SIZE 16
+#define HEADERX_FILE_SIZE 48
 
 int parseHeader(FILE *file, struct Header *header) {
     char buffer[HEADER_FILE_SIZE];
@@ -24,8 +26,62 @@ int parseHeader(FILE *file, struct Header *header) {
     return 0;
 }
 
+uint16_t readWord16LittleEndian(const void *addr) {
+    const unsigned char *casted = (const unsigned char *) addr;
+    uint16_t result = casted[1];
+    return (result << 8) + casted[0];
+}
+
+uint32_t readWord32LittleEndian(const void *addr) {
+    const unsigned char *casted = (const unsigned char *) addr;
+    uint32_t result = casted[3];
+    for (int i = 2; i >= 0; i--) {
+        result = (result << 8) + casted[i];
+    }
+
+    return result;
+}
+
+uint64_t readWord64LittleEndian(const void *addr) {
+    const unsigned char *casted = (const unsigned char *) addr;
+    uint64_t result = casted[7];
+    for (int i = 6; i >= 0; i--) {
+        result = (result << 8) + casted[i];
+    }
+
+    return result;
+}
+
+int parseHeaderX(FILE *file, struct HeaderX *headerX) {
+    char buffer[HEADERX_FILE_SIZE];
+    if (fread(buffer, 1, HEADERX_FILE_SIZE, file) < HEADERX_FILE_SIZE) {
+        fprintf(stderr, "Unexpected end of file\n");
+        return 1;
+    }
+
+    headerX->type = readWord16LittleEndian(buffer);
+    headerX->machine = readWord16LittleEndian(buffer + 2);
+    headerX->version = readWord32LittleEndian(buffer + 4);
+    headerX->entry = readWord64LittleEndian(buffer + 8);
+    headerX->programHeaderTable = readWord64LittleEndian(buffer + 16);
+    headerX->sectionHeaderTable = readWord64LittleEndian(buffer + 24);
+    headerX->flags = readWord32LittleEndian(buffer + 32);
+    headerX->headerSize = readWord16LittleEndian(buffer + 36);
+    headerX->programHeaderTableEntrySize = readWord16LittleEndian(buffer + 38);
+    headerX->programHeaderTableEntryCount = readWord16LittleEndian(buffer + 40);
+    headerX->sectionHeaderTableEntrySize = readWord16LittleEndian(buffer + 42);
+    headerX->sectionHeaderTableEntryCount = readWord16LittleEndian(buffer + 44);
+    headerX->sectionHeaderTableStringTableIndex = readWord16LittleEndian(buffer + 46);
+
+    return 0;
+}
+
 void dumpHeader(const struct Header *header) {
-    printf("Header(class=%u, data=%u, version=%u, osabi=%u, abiVersion=%u)", (int) header->class, (int) header->data, (int) header->version, (int) header->osabi, (int) header->abiVersion);
+    printf("Header:\n  class=%u\n  data=%u\n  version=%u\n  osabi=%u\n  abiVersion=%u\n", (int) header->class, (int) header->data, (int) header->version, (int) header->osabi, (int) header->abiVersion);
+}
+
+void dumpHeaderX(const struct HeaderX *headerX) {
+    printf("HeaderX:\n  type=%u\n  machine=%u\n  version=%u\n  entry=%lu\n  programHeaderTable=%lu\n  sectionHeaderTable=%lu\n  flags=%u\n  headerSize=%u\n  programHeaderTableEntrySize=%u\n  programHeaderTableEntryCount=%u\n  sectionHeaderTableEntrySize=%u\n  sectionHeaderTableEntryCount=%u\n  sectionHeaderTableStringTableIndex=%u\n", (int) headerX->type, (int) headerX->machine, (int) headerX->version, headerX->entry, headerX->programHeaderTable, headerX->sectionHeaderTable, (int) headerX->flags, (int) headerX->headerSize, (int) headerX->programHeaderTableEntrySize, (int) headerX->programHeaderTableEntryCount, (int) headerX->sectionHeaderTableEntrySize, (int) headerX->sectionHeaderTableEntryCount, (int) headerX->sectionHeaderTableStringTableIndex);
 }
 
 int main(int argc, const char *argv[]) {
@@ -36,13 +92,31 @@ int main(int argc, const char *argv[]) {
     }
 
     struct Header header;
-    const int result = parseHeader(file, &header);
-    fclose(file);
-
-    if (!result) {
-        dumpHeader(&header);
-        printf("\n");
+    if (parseHeader(file, &header)) {
+        fclose(file);
+        return 1;
     }
 
-    return result;
+    dumpHeader(&header);
+
+    // For now we only focus in 64-bit little endian files
+    if (header.class != 2) {
+        fprintf(stderr, "Unexpected ELF file. It was expected a 64-bit one\n");
+        return 1;
+    }
+
+    if (header.data != 1) {
+        fprintf(stderr, "Unexpected ELF file. It was expected a little endian one\n");
+        return 1;
+    }
+
+    struct HeaderX headerX;
+    if (parseHeaderX(file, &headerX)) {
+        fclose(file);
+        return 1;
+    }
+
+    dumpHeaderX(&headerX);
+    fclose(file);
+    return 0;
 }
